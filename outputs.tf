00bec4fc408162e61ec3ad88a5c672e652eded1e
David Goodell <dgoodell@rhythmictech.com>
2021-05-03 17:15:20 -0400

    add iam role name to outputs

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,8 @@ output "iam_role_arn" {
   description = "ARN of the IAM Role generated for this instance"
   value       = aws_iam_role.instance[0].arn
 }
+
+output "iam_role_name" {
+  description = "Name of the IAM Role generated for this instance"
+  value       = aws_iam_role.instance[0].name
+}
