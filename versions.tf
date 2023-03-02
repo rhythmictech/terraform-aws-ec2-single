2ec02bea161f51c4c02be9dccca9043b33351f03
Steven Black <genseb7@gmail.com>
2023-03-02 09:43:24 -0500

    relax version constraint

--- versions.tf
+++ versions.tf
@@ -4,12 +4,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.45.0, < 4.0.0"
+      version = ">= 2.45.0"
     }
 
     tls = {
       source  = "hashicorp/tls"
-      version = ">= 3.1.0, < 4.0.0"
+      version = ">= 3.1.0"
     }
   }
 }
